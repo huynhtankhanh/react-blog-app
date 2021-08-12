b9850c91d8a78f8634f56b7bf2eec5a5c49a2bf1
Khanh Huynh <tankhanhhuynh@gmail.com>
2021-08-12 22:16:27 +0700

    Adding environment variable

--- server/index.js
+++ server/index.js
@@ -1,27 +1,31 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import cors from 'cors';
-import posts from './routers/posts.js';
-import mongoose from 'mongoose';
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import posts from "./routers/posts.js";
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(bodyParser.json({ limit: '30mb' }));
-app.use(bodyParser.urlencoded({ extended: true, limit: '30mb' }));
+app.use(bodyParser.json({ limit: "30mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "30mb" }));
 app.use(cors());
 
 //using routers
-app.use('/posts', posts);
+app.use("/posts", posts);
 
-const URI = 'mongodb+srv://htkhanh:htkhanh@cluster0.kjvj2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+dotenv.config();
+const URI = process.env.DB_URL;
 
-mongoose.connect(URI, {useNewUrlParser: true, useUnifiedTopology: true})
-.then(() => {
-    console.log('Connected to DB');
+mongoose
+  .connect(URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to DB");
     app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    })
-}).catch(err => {
-    console.log('err', err);
-})
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("err", err);
+  });
